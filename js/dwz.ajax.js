43c9463b068b04c4294323a7da7ac7737e2a1f36
fei.morpheus.ma <fei.morpheus.ma@gmail.com>
2017-06-06 22:59:56 +0800

    aa

--- js/dwz.ajax.js
+++ js/dwz.ajax.js
@@ -291,7 +291,7 @@ function ajaxTodo(url, callback){
 	var $callback = callback || navTabAjaxDone;
 	if (! $.isFunction($callback)) $callback = eval('(' + callback + ')');
 	$.ajax({
-		type:'POST',
+		type:'GET',
 		url:url,
 		dataType:"json",
 		cache: false,
